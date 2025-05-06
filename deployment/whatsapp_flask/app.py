e8895615fc853786b1317de457d3cb5c8494c47e
Galuh Alifani <galuh.adika@gmail.com>
2025-05-06 12:23:23 +0700

    add try except

--- deployment/whatsapp_flask/app.py
+++ deployment/whatsapp_flask/app.py
@@ -21,80 +21,87 @@ def root():
 
 @app.route("/whatsapp", methods=["POST"])
 def whatsapp_webhook():
-    incoming_msg = request.values.get("Body", "").strip()
-    user_id = request.values.get("From", "").strip()
+    try:
+        incoming_msg = request.values.get("Body", "").strip()
+        user_id = request.values.get("From", "").strip()
 
-    result = check_question_feedback(incoming_msg, user_id)
-    last_qna = result["last_qna"]
+        result = check_question_feedback(incoming_msg, user_id)
+        last_qna = result["last_qna"]
 
-    resp = MessagingResponse()
-    user = check_user(user_id)
-    new_user = user['status'] == 'new'
+        resp = MessagingResponse()
+        user = check_user(user_id)
+        new_user = user['status'] == 'new'
 
-    lang = detect(incoming_msg)
+        lang = detect(incoming_msg)
 
-    if new_user:
-        print(f"########### Send initial greetings: {user_id}")
-        translated_greeting = translate_text(lang, greeting)
-        resp.message(translated_greeting)
+        if new_user:
+            print(f"########### Send initial greetings: {user_id}")
+            translated_greeting = translate_text(lang, greeting)
+            resp.message(translated_greeting)
 
-    if len(incoming_msg) > 150:
-        exceed_length_resp = "Sorry, your message is too long. Please shorten it to less than 150 characters."
-        reply = translate_text(lang, exceed_length_resp)
-        resp.message(reply)
-        return str(resp)
-    
-    if result["is_feedback"]:
-        if not last_qna["question"]:
-            reply = "Sorry, please ask a question first before providing feedback."
-        else:
-            reply = save_feedback(result["feedback_obj"], last_qna)
-
-        resp.message(reply)
-        return str(resp)
-    
-    is_balance_available = check_user_balance(user)
-    if not is_balance_available:
-        reply = translate_text(lang, "Sorry, you have reached your daily conversation limit. You can start a new conversation tomorrow.")
-        resp.message(reply)
-        return str(resp)
-    else:
-        if len(incoming_msg) > 4:
-            # send an immediate placeholder response
-            if not last_qna["question"]:
-                message = translate_text(lang, 'let me check that for you...')
-                resp.message(f"⏳ {message}")
-
-        def process_response():
-            print(f"########### Running process_response for user: {user_id}")
-            reply = ask(incoming_msg, user_id)
-            
-            if not reply:
-                reply = translate_text(lang, "Sorry, I missed that - can you please try asking again?")
+        if len(incoming_msg) > 150:
+            exceed_length_resp = "Sorry, your message is too long. Please shorten it to less than 150 characters."
+            reply = translate_text(lang, exceed_length_resp)
+            resp.message(reply)
+            return str(resp)
         
-            # send the actual message via Twilio API
-            client = Client(os.getenv("TWILIO_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
-            if len(reply) > 1599:
-                for part in split_message(reply):
+        if result["is_feedback"]:
+            if not last_qna["question"]:
+                reply = "Sorry, please ask a question first before providing feedback."
+            else:
+                reply = save_feedback(result["feedback_obj"], last_qna)
+
+            resp.message(reply)
+            return str(resp)
+        
+        is_balance_available = check_user_balance(user)
+        if not is_balance_available:
+            reply = translate_text(lang, "Sorry, you have reached your daily conversation limit. You can start a new conversation tomorrow.")
+            resp.message(reply)
+            return str(resp)
+        else:
+            if len(incoming_msg) > 4:
+                # send an immediate placeholder response
+                if not last_qna["question"]:
+                    message = translate_text(lang, 'let me check that for you...')
+                    resp.message(f"⏳ {message}")
+
+            def process_response():
+                print(f"########### Running process_response for user: {user_id}")
+                reply = ask(incoming_msg, user_id)
+                
+                if not reply:
+                    reply = translate_text(lang, "Sorry, I missed that - can you please try asking again?")
+            
+                # send the actual message via Twilio API
+                client = Client(os.getenv("TWILIO_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
+                if len(reply) > 1599:
+                    for part in split_message(reply):
+                        client.messages.create(
+                            from_=os.getenv("TWILIO_PHONE_NUMBER"),
+                            to=user_id,
+                            body=part
+                        )
+                else:
                     client.messages.create(
                         from_=os.getenv("TWILIO_PHONE_NUMBER"),
                         to=user_id,
-                        body=part
+                        body=reply
                     )
-            else:
-                client.messages.create(
-                    from_=os.getenv("TWILIO_PHONE_NUMBER"),
-                    to=user_id,
-                    body=reply
-                )
-            
-            # deduct balance
-            try:
-                deduct_chat_balance(user, user_id)
-            except Exception as e:
-                print(f"Error deducting chat balance: {str(e)}")
+                
+                # deduct balance
+                try:
+                    deduct_chat_balance(user, user_id)
+                except Exception as e:
+                    print(f"Error deducting chat balance: {str(e)}")
 
-        Thread(target=process_response).start()
+            Thread(target=process_response).start()
+            return str(resp)
+        
+    except Exception as e:
+        print("❌ Internal server error:")
+        resp = MessagingResponse()
+        resp.message(f"We are sorry, the bot is currently unavailable or under maintenance. Please try again later.")
         return str(resp)
 
 @app.route("/sandbox", methods=["POST"])
